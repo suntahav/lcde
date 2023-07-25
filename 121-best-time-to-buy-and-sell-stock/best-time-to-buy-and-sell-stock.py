517b8b789b74dc801e72cf26120a9ef6ab64335b
Sandeep <vijeta013@gmail.com>
2023-07-25 03:19:40 -0400

    Time: 1058 ms (33.11%) | Memory: 27.3 MB (94.05%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -5,14 +5,13 @@ class Solution:
         rp = 1
         max_profit = 0
         while rp < len(prices):
-            if prices[rp] == prices[lp]:
-                rp += 1
-            elif prices[rp] < prices[lp]:
-                lp = rp
-                rp += 1
-            else :
+            if prices[rp] > prices[lp]:
                 max_profit = max(max_profit, prices[rp] - prices[lp])
                 rp += 1
+            else:
+                lp = rp
+                rp += 1
+                
             
             
         return max_profit    
