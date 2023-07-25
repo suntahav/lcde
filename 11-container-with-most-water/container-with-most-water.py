b11a2ebaeb7c8d8355162f253c04967972cf2575
Sandeep <vijeta013@gmail.com>
2023-07-25 01:38:07 -0400

    Time: 734 ms (53.76%) | Memory: 29.1 MB (91.48%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,13 +1,13 @@
 class Solution:
     def calculate_vol(self, i_1, i_2, j_1, j_2):
-        return min(j_1, j_2) * abs(i_1 - i_2)
+        return 
 
     def maxArea(self, height: List[int]) -> int:
         l_p = 0
         r_p = len(height) - 1
         max_vol = 0
         while l_p < r_p:
-            vol = self.calculate_vol(l_p, r_p, height[l_p], height[r_p])
+            vol = min(height[l_p], height[r_p]) * abs(l_p - r_p)
             if vol > max_vol:
                 max_vol = vol
             if height[l_p] < height[r_p]:
