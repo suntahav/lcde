bbba72cac90f639112606b90d49b93386b1d07f3
Sandeep <vijeta013@gmail.com>
2023-07-26 13:08:30 -0400

    Time: 24 ms (24.60%) | Memory: 13.6 MB (24.93%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for i in range(len(s)):
+            if len(stack) == 0:
+                stack.append(s[i])
+            elif (ord(stack[-1]) == ord(s[i])-1) or (ord(stack[-1]) == ord(s[i])-2):
+                stack.pop(len(stack)-1)
+            else:
+                stack.append(s[i])
+        return len(stack) == 0
