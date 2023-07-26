2a1b0ea964ddfb0e00daf3a8ee41e1f257b05bc6
Sandeep <vijeta013@gmail.com>
2023-07-26 13:10:40 -0400

    Time: 16 ms (76.04%) | Memory: 13.5 MB (24.93%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -9,7 +9,7 @@ class Solution(object):
             if len(stack) == 0:
                 stack.append(s[i])
             elif (ord(stack[-1]) == ord(s[i])-1) or (ord(stack[-1]) == ord(s[i])-2):
-                stack.pop(len(stack)-1)
+                stack.pop()
             else:
                 stack.append(s[i])
         return len(stack) == 0
