fa0e61e120a2d434d6f4deeece6a6eb81555f6f9
Sandeep <vijeta013@gmail.com>
2023-08-06 02:48:39 -0400

    Time: 41 ms (97.95%) | Memory: 16.6 MB (80.86%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -6,11 +6,15 @@ class Solution:
             mid = (lp + rp) // 2
             if mid == lp:
                 break
-            
+            #if immediate next of mid less than mid then that is least due to flip
             if nums[mid] > nums[mid + 1]:
                 return nums[mid + 1]
+            
+            # if mid is greater than rp means a flip somewhere in between so move lp ahead
             elif nums[mid] > nums[rp]:
                 lp = mid
+            # if mid is less than rp means it behaves as normal ascending array so less element should be before hence move rp to mid
             else:
                 rp = mid
+            # return min in case mid becomes equal to lp
         return min(nums[lp], nums[rp])
