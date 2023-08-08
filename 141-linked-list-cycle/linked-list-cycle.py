faebb9f6e8f72869211881758482f1fa831101c1
Sandeep <vijeta013@gmail.com>
2023-08-08 19:08:39 -0400

    Time: 56 ms (92.90%) | Memory: 20.4 MB (66.40%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -7,11 +7,22 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         fp = head
-        while fp is not None:
-            if fp.val == 100001:
+        # while fp is not None:
+        #     if fp.val == 100001:
+        #         return True
+        #     else:
+        #         fp.val = 100001
+        #         fp = fp.next
+
+        # floyd cycle detection
+        sp = head
+        if fp == None:
+            return False
+        while (fp is not None) and (fp.next is not None):
+            fp = fp.next.next
+            sp = sp.next
+
+            if fp == sp:
                 return True
-            else:
-                fp.val = 100001
-                fp = fp.next
         
         return False
