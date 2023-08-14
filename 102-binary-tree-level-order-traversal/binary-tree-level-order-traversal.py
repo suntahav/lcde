f69b0807ba1f25e62e538912c4b843fbaa0473dd
Sandeep <vijeta013@gmail.com>
2023-08-14 05:55:55 -0400

    Time: 49 ms (70.43%) | Memory: 17.1 MB (68.51%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -5,25 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def bfs(self, arr, root, level=0):
-        if root is None:
-            return
-        #add the number to the 2d array at specific level
-        arr[level].append(root.val)
-        #traverse left and right with increased level
-        if root.left is not None:
-            self.bfs(arr, root.left, level+1)
-        if root.right is not None:
-            self.bfs(arr, root.right, level+1)
-        
-         
-
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        #2d array for storing the numbers for each level : max is 2000 as a skewed tree
-        arr = [[] for x in range(2001)]
-        self.bfs(arr, root, level=0)
+        #Queue approach
         res = []
-        for i in range(2001):
-            if len(arr[i]) != 0:
-                res.append(arr[i])
+        queue = collections.deque()
+        queue.append(root)
+        while queue:
+            qlen = len(queue)
+            level = []
+            for i in range(qlen):
+                item = queue.popleft()
+                if item:
+                    level.append(item.val)
+                    queue.append(item.left)
+                    queue.append(item.right)
+            
+            if level:
+                res.append(level)
         return res
+            
