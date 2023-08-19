38712451c9b0eb856d2e7dfd2e65afc0b4a66c71
Sandeep <vijeta013@gmail.com>
2023-08-19 02:08:03 -0400

    Time: 60 ms (60.74%) | Memory: 20.5 MB (14.47%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -5,13 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inorderTraverse(self, root, arr):
+    def inorderTraverse(self, root, arr,k):
+        if len(arr) == k:
+            return
         if root:
-            self.inorderTraverse(root.left, arr)
+            self.inorderTraverse(root.left, arr, k)
+            if len(arr) == k:
+                return
             arr.append(root.val)
-            self.inorderTraverse(root.right, arr)
+            if len(arr) == k:
+                return
+            self.inorderTraverse(root.right, arr, k)
+            if len(arr) == k:
+                return
         return
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        arr = []
-        self.inorderTraverse(root, arr)
-        return arr[k-1]
+        arr = deque()
+        self.inorderTraverse(root, arr,k)
+        return arr.pop()
