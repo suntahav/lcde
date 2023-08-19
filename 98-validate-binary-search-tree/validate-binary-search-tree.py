12668573ae2335532f5e781044f69c2be279ba72
Sandeep <vijeta013@gmail.com>
2023-08-19 01:36:32 -0400

    Time: 53 ms (64.86%) | Memory: 18.7 MB (74.16%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,10 +6,14 @@
 #         self.right = right
 class Solution:
     def _isValidBST(self, root, mx=None, mn=None):
+        #Define the left and right limit of each node and check recursively
         if root is None:
+            #root doesnt exist means true
             return True
-        if root.val < mx and root.val > mn:
+        if root.val < mx and root.val > mn: # in range
+            #for left the min remains same but max is minimum or root node and original max
             left = self._isValidBST(root.left, mn = mn, mx = min(root.val, mx))
+            #for right the max remains same but minimum must be greater than the max of root and the original minimum
             right = self._isValidBST(root.right, mn = max(mn, root.val), mx = mx)
             if left and right:
                 return True
