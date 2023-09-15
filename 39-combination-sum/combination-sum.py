0f21bc531db77bd42e3b50f50eb8322f00ea0ef9
Sandeep <vijeta013@gmail.com>
2023-09-15 04:37:27 -0400

    Time: 71 ms (33.45%) | Memory: 16.4 MB (53.41%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
         #Could have a case where candidates are duplicate
-        candidates = list(set(candidates))
+        # candidates = list(set(candidates))
 
         def calculate(target, current, idx):
             
