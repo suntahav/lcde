240129bb16f27ea1726ff6e09c272523b4e5acb7
Sandeep <vijeta013@gmail.com>
2023-09-23 21:21:12 -0400

    Time: 33 ms (90.85%) | Memory: 16.2 MB (89.92%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,13 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        mem = [0] * len(nums)
+        mem = [-1] * len(nums)
+        
         mem[0] = nums[0]
         if len(nums) >= 2:
             mem[1] = max(mem[0], nums[1])
-        else:
-            return mem[0]
-        for i in range(2, len(nums)):
-            mem[i] = max(mem[i-1] , mem[i-2] + nums[i])
         
-        return max(mem)
+        def dp(idx):
+            if idx == 0:
+                return mem[0]
+            elif idx == 1:
+                return mem[1]
+            elif mem[idx] != -1:
+                return mem[idx]
+            else:
+                res = max(nums[idx]+dp(idx-2), dp(idx-1))
+                mem[idx] = res
+                return res
         
+        return dp(len(nums)-1)
