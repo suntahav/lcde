8927c9755cbae2b65d7a7ccebd732a588fbae122
Sandeep <vijeta013@gmail.com>
2023-09-24 01:31:45 -0400

    Time: 32 ms (90.63%) | Memory: 16.1 MB (97.38%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,11 +1,6 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        mapper = lambda x : int(x)
-        binary = str(bin(n))[2:]
-        # print(binary)
-        bin_list = list(binary)
-        # print(bin_list)
-        res = sum(map(mapper, bin_list))
-        return res
+        return bin(n)[2:].count('1')
+
         
         
