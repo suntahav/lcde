919cb34e516f43ad579305aad069872240383f0c
Sandeep <vijeta013@gmail.com>
2023-09-23 21:23:03 -0400

    Time: 34 ms (87.75%) | Memory: 16.3 MB (65.55%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -7,15 +7,18 @@ class Solution:
             mem[1] = max(mem[0], nums[1])
         
         def dp(idx):
+            #first two values of memoized are available from prior calculation
             if idx == 0:
                 return mem[0]
             elif idx == 1:
                 return mem[1]
             elif mem[idx] != -1:
+                #return Memoized values
                 return mem[idx]
             else:
+                #run dp from end to back and store the results
                 res = max(nums[idx]+dp(idx-2), dp(idx-1))
                 mem[idx] = res
                 return res
-        
+        #we are going from end to first so call at end
         return dp(len(nums)-1)
