8ef0b115059628e52567d87ba32c71e6654adda6
Sandeep <vijeta013@gmail.com>
2023-10-31 19:16:43 -0400

    Time: 670 ms (48.02%) | Memory: 36.3 MB (72.41%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.py
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.py
@@ -1,11 +1,10 @@
 class Solution:
     def findArray(self, pref: List[int]) -> List[int]:
         res = [0] * len(pref)
-        xor_cum = [0] * len(pref)
         res[0] = pref[0]
-        xor_cum[0] = pref[0]
+        xor_cum = pref[0]
         for i in range(1, len(pref)):
-            res[i] = xor_cum[i-1] ^ pref[i]
-            xor_cum[i] = res[i] ^ xor_cum[i-1]
+            res[i] = xor_cum ^ pref[i]
+            xor_cum = res[i] ^ xor_cum
         return res
         
