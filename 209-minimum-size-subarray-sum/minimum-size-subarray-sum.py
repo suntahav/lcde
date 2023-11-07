d7f033ad1ca0c867bcc8bb9a2d631cf63773ae80
Sandeep <vijeta013@gmail.com>
2023-11-07 02:33:09 -0500

    Time: 240 ms (14.87%) | Memory: 29.5 MB (60.19%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,26 +1,43 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        min_len = float('inf')
-        left = 0
-        right = 1
-        LEN = len(nums)
-        temp_sum = sum(nums[left:right]) 
-        while left <= right and right <= LEN:
-            # print(left, right, temp_sum)
+        # min_len = float('inf')
+        # left = 0
+        # right = 1
+        # LEN = len(nums)
+        # temp_sum = sum(nums[left:right]) 
+        # while left <= right and right <= LEN:
+        #     # print(left, right, temp_sum)
             
-            if temp_sum >= target:
-                if min_len > (right - left):
-                    min_len = right - left
+        #     if temp_sum >= target:
+        #         if min_len > (right - left):
+        #             min_len = right - left
                 
-            if temp_sum >= target:
-                temp_sum -= nums[left]
+        #     if temp_sum >= target:
+        #         temp_sum -= nums[left]
+        #         left += 1
+        #     else:
+        #         if right < LEN:
+        #             temp_sum += nums[right]
+        #         right += 1
+        # if min_len == float('inf'):
+        #     min_len = 0
+        # return min_len
+
+        total = 0
+        left = 0
+        right = 0
+        LEN = len(nums)
+        min_len = float("inf")
+        while right < LEN and left <= right:
+            total += nums[right]
+            while total >= target:
+                min_len = min(min_len, right-left+1)
+                total -= nums[left]
                 left += 1
-            else:
-                if right < LEN:
-                    temp_sum += nums[right]
-                right += 1
-        if min_len == float('inf'):
-            min_len = 0
+            right += 1
+        
+        if min_len == float("inf"):
+            return 0
         return min_len
 
         
