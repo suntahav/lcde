b593fbaafa7b91f53357af85a60a4085bda903c3
Sandeep <vijeta013@gmail.com>
2023-11-14 00:39:31 -0500

    Time: 201 ms (69.30%) | Memory: 22 MB (5.26%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -2,19 +2,31 @@ class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
         mapper = {}
 
-        unq_chars = set(s)
+        # unq_chars = set(s)
         LEN = len(s)
-        for c in unq_chars:
-            start_idx = s.index(c)
-            counter = 0
-            last_idx = start_idx
-            temp = set()
-            for i in range(start_idx+1, LEN):
-                if s[i] == c:
-                    if c not in temp and last_idx != start_idx:
-                        temp.add(c)
-                    mapper[c] = len(temp)
-                    last_idx = i
-                else:
-                    temp.add(s[i])
-        return sum((mapper.values()))
+        # for c in unq_chars:
+        #     start_idx = s.index(c)
+        #     counter = 0
+        #     last_idx = start_idx
+        #     temp = set()
+        #     for i in range(start_idx+1, LEN):
+        #         if s[i] == c:
+        #             if c not in temp and last_idx != start_idx:
+        #                 temp.add(c)
+        #             mapper[c] = len(temp)
+        #             last_idx = i
+        #         else:
+        #             temp.add(s[i])
+        # return sum((mapper.values()))
+        for i,c in enumerate(s):
+            if c in mapper:
+                mapper[c].append(i)
+            else:
+                mapper[c] = [i]
+        res = 0
+        for c in mapper.keys():
+            if len(mapper[c]) < 2:
+                continue
+            else:
+                res += len(set(s[mapper[c][0]+1 : mapper[c][-1]]))
+        return res
