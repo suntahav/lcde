9cf5456c7621ec22c1f0c28ffef28c762c196fad
Sandeep <vijeta013@gmail.com>
2023-11-23 06:02:52 -0500

    Time: 164 ms (67.04%) | Memory: 16.6 MB (39.64%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.py
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -4,12 +4,17 @@ class Solution:
         res = []
         for start, end in zip(l,r):
             temp = nums[start:end+1]
-            temp = sorted(temp)
-            temp = [temp[i]-temp[i-1] for i in range(1, len(temp))]
-            if max(temp) == min(temp):
-                res.append(True)
-            else:
+            if len(temp) < 2:
                 res.append(False)
+                continue
+            temp.sort()
+            diff = temp[1]-temp[0]
+            temp_res = True
+            for i in range(1, len(temp)):
+                if temp[i] - temp[i-1] != diff:
+                    temp_res = False
+                    break
+            res.append(temp_res)
         return res
 
 
