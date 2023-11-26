cda63243140096d0a22cc5bdba9a5b97b5589edf
Sandeep <vijeta013@gmail.com>
2023-11-25 19:06:46 -0500

    Time: 59 ms (68.88%) | Memory: 16.5 MB (37.07%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,20 +1,27 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dp = {}
-        def calculate(idx, cost):
-            #if already exist
-            if idx in dp:
-                return dp[idx]
-            #base case
-            if idx == len(cost)-1:
-                dp[idx] = cost[-1]
-                return cost[-1]
+        # dp = {}
+        # def calculate(idx, cost):
+        #     #if already exist
+        #     if dp[idx] != -1:
+        #         return dp[idx]
+        #     #base case
+        #     if idx == len(cost)-1:
+        #         dp[idx] = cost[-1]
+        #         return cost[-1]
             
-            if idx == len(cost)-2:
-                dp[idx] = cost[-2]
-                return cost[-2]
-            dp[idx] = cost[idx] + min(calculate(idx+1, cost), calculate(idx+2, cost))
-            return  dp[idx]
+        #     if idx == len(cost)-2:
+        #         dp[idx] = cost[-2]
+        #         return cost[-2]
+        #     dp[idx] = cost[idx] + min(calculate(idx+1, cost), calculate(idx+2, cost))
+        #     return  dp[idx]
         
-        return min(calculate(0, cost), calculate(1, cost))
+        # return min(calculate(0, cost), calculate(1, cost))
         
+        
+        LEN = len(cost)
+        for i in range(2, LEN):
+            m = min(cost[i-2], cost[i-1])
+            cost[i] += m
+        return min(cost[-1], cost[-2])
+
