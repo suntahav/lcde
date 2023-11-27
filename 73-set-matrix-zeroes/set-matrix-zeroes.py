19dfcadbc0675ed2bc1042b3653ec7907ec4ea30
Sandeep <vijeta013@gmail.com>
2023-11-26 23:31:25 -0500

    Time: 119 ms (44.00%) | Memory: 17.4 MB (7.29%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,19 +5,33 @@ class Solution:
         """
         ROWS = len(matrix)
         COLS = len(matrix[0])
-        # O(m+n)
-        rows = [1] * ROWS
-        cols = [1] * COLS
+        
+        flag_row = False
+        flag_col = False
+        for i in range(COLS):
+            if matrix[0][i] == 0:
+                flag_row = True
+        for i in range(ROWS):
+            if matrix[i][0] == 0:
+                flag_col = True
 
         #handle rows 
         for i in range(ROWS):
             for j in range(COLS):
                 if matrix[i][j] == 0:
-                    rows[i] = 0
-                    cols[j] = 0
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
         
         #Inplace operation
-        for i in range(ROWS):
-            for j in range(COLS):
-                if rows[i] == 0 or cols[j] == 0:
+        
+        for i in range(1, ROWS):
+            for j in range(1,COLS):
+                if matrix[i][0] == 0 or matrix[0][j]==0:
                     matrix[i][j] = 0
+        
+        if flag_row:
+            for i in range(COLS):
+                matrix[0][i] = 0
+        if flag_col:
+            for i in range(ROWS):
+                matrix[i][0] = 0
