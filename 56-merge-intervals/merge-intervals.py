71cbf12ea2fcf65751408ad40d02d72498a221df
Sandeep <vijeta013@gmail.com>
2023-12-03 05:17:58 -0500

    Time: 130 ms (86.98%) | Memory: 21.1 MB (67.93%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,10 +4,7 @@ class Solution:
         res = [arr[0]]
         for i in range(1, len(arr)):
             if arr[i][0] <= res[-1][1]:
-                if res[-1][1] <= arr[i][1]:
-                    res[-1][1] = arr[i][1]
-                else:
-                    continue
+                res[-1][1] = max(arr[i][1], res[-1][1])
             else:
                 res.append(arr[i])
         return res
