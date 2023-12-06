a6c4d6eb6ace8069fbcf3ea1a24a7568cbc6aaf1
Sandeep <vijeta013@gmail.com>
2023-12-06 03:41:34 -0500

    Time: 403 ms (89.59%) | Memory: 38.4 MB (34.93%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -4,23 +4,27 @@ class MedianFinder:
     def __init__(self):
         self.left = []
         self.right = []
-        heapq.heapify(self.left)
-        heapq.heapify(self.right)
+        # heapq.heapify(self.left)
+        # heapq.heapify(self.right)
         
 
     def addNum(self, num: int) -> None:
-        if len(self.right) == 0 and len(self.left) == 0:
-            heapq.heappush(self.right, num)
+        # if len(self.right) == 0 and len(self.left) == 0:
+        #     heapq.heappush(self.right, num)
+        # else:
+        #     if num >= self.right[0]:
+        #         heapq.heappush(self.right, num)
+        #     else:
+        #         heapq.heappush(self.left, -num)
+        # while abs(len(self.right) - len(self.left)) > 1:
+        #     if len(self.right) > len(self.left):
+        #         heapq.heappush(self.left, -heapq.heappop(self.right))
+        #     else:
+        #         heapq.heappush(self.right, -heapq.heappop(self.left))
+        if len(self.left)==len(self.right):
+            heapq.heappush(self.right, -heapq.heappushpop(self.left, -num))
         else:
-            if num >= self.right[0]:
-                heapq.heappush(self.right, num)
-            else:
-                heapq.heappush(self.left, -num)
-        while abs(len(self.right) - len(self.left)) > 1:
-            if len(self.right) > len(self.left):
-                heapq.heappush(self.left, -heapq.heappop(self.right))
-            else:
-                heapq.heappush(self.right, -heapq.heappop(self.left))
+            heapq.heappush(self.left, -heapq.heappushpop(self.right, num))
 
             
             
