26fd0b025f06439a37d1036d028dc83bb990a443
Sandeep <vijeta013@gmail.com>
2023-12-19 01:43:45 -0500

    Time: 1268 ms (20.84%) | Memory: 18.5 MB (27.37%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,23 +1,27 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        INT_MAX = 2**31
+        INT_MAX = 2**32
         LEN = len(coins)
-        matrix = [[0] * (amount+1) for i in range(LEN+1)]
+        t = [[-1]*(amount+1) for i in range(LEN+1)]
 
-        for i in range( amount+1):
-            matrix[0][i] = INT_MAX
-        for i in range(1, amount+1):
-            if i%coins[0] == 0:
-                matrix[1][i] = i // coins[0]
+        # for i in range(LEN+1):
+        #     t[i][0] = 0
+        for i in range(amount+1):
+            t[0][i] = INT_MAX
+        for i in range(amount+1):
+            if i %coins[0]==0:
+                t[1][i] = i // coins[0]
             else:
-                matrix[1][i] = INT_MAX
-        
-        for i in range(1, LEN+1):
-            for j in range(1, amount+1):
+                t[1][i] = INT_MAX
+        for i in range(LEN+1):
+            t[i][0]=0
+        for i in range(2, LEN+1):
+            for j in range(1,amount+1):
                 if coins[i-1] <= j:
-                    matrix[i][j] = min(matrix[i][j-coins[i-1]] + 1, matrix[i-1][j])
+                    t[i][j] = min(t[i-1][j], 1+t[i][j-coins[i-1]])
                 else:
-                    matrix[i][j] = matrix[i-1][j]
-        if matrix[LEN][amount] >= INT_MAX:
+                    t[i][j] = t[i-1][j]
+        if t[LEN][amount] >= 2**32:
             return -1
-        return matrix[LEN][amount]
+        return t[LEN][amount]
+        
