fd982702981e363976312993269faf783bb26eb2
Sandeep <vijeta013@gmail.com>
2023-12-18 21:50:05 -0500

    Time: 418 ms (79.60%) | Memory: 17.1 MB (59.70%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -2,37 +2,39 @@ class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
         ROW = len(img)
         COL = len(img[0])
+        ROW_R = range(ROW)
+        COL_R = range(COL)
         res = []
-        for i in range(ROW):
+        for i in ROW_R:
             temp = []
-            for j in range(COL):
+            for j in COL_R:
                 a = 0
                 c = 0
-                if i-1 in range(ROW):
-                    if j-1 in range(COL):
+                if i-1 in ROW_R:
+                    if j-1 in COL_R:
                         a += img[i-1][j-1]
                         c += 1
                     a += img[i-1][j]
                     c += 1
-                    if j+1 in range(COL):
+                    if j+1 in COL_R:
                         a+= img[i-1][j+1]
                         c += 1
-                if i+1 in range(ROW):
-                    if j-1 in range(COL):
+                if i+1 in ROW_R:
+                    if j-1 in COL_R:
                         a += img[i+1][j-1]
                         c += 1
                     a += img[i+1][j]
                     c += 1
 
-                    if j+1 in range(COL):
+                    if j+1 in COL_R:
                         a+= img[i+1][j+1]
                         c += 1
-                if j-1 in range(COL):
+                if j-1 in COL_R:
                     a += img[i][j-1]
                     c += 1
                 a += img[i][j]
                 c += 1
-                if j+1 in range(COL):
+                if j+1 in COL_R:
                     a+= img[i][j+1]
                     c += 1
                 temp.append(a//c)
