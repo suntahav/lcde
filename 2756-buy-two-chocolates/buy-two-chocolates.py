8d5347818a521b5341b482d67a77d3bb0812062b
Sandeep <vijeta013@gmail.com>
2023-12-19 21:08:34 -0500

    Time: 76 ms (9.00%) | Memory: 16.2 MB (55.48%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -1,5 +1,9 @@
+import heapq
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        val = money - sum(sorted(prices)[:2])
+        heapq.heapify(prices)
+        a = heapq.heappop(prices)
+        a += heapq.heappop(prices)
+        val = money - a
         return val if val >= 0 else money
         
