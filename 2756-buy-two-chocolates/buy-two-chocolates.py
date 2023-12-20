2563685b0323c8e96bcbf18a424750023b744219
Sandeep <vijeta013@gmail.com>
2023-12-19 21:11:12 -0500

    Time: 77 ms (7.73%) | Memory: 16.2 MB (55.48%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -1,9 +1,13 @@
 import heapq
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        heapq.heapify(prices)
-        a = heapq.heappop(prices)
-        a += heapq.heappop(prices)
-        val = money - a
+        f,s = 101,101
+        for p in prices:
+            if p < f:
+                s = f
+                f = p
+            elif p<s:
+                s=p
+        val = money -f-s
         return val if val >= 0 else money
         
