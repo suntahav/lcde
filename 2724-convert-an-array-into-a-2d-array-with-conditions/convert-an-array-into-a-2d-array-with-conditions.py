e4a1476340db65e10d5e4155721a283207204052
Sandeep <vijeta013@gmail.com>
2024-01-01 23:25:16 -0500

    Time: 131 ms (5.01%) | Memory: 17.4 MB (12.28%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -6,10 +6,8 @@ class Solution:
         c = Counter(nums)
         max_val = c[max(c, key=c.get)]
         res = [[]for i in range(max_val)]
-        # print(res)
         for k in c.keys():
             for i in range(c[k]):
-                # print(i)
                 res[i].append(k)
         
         return res
