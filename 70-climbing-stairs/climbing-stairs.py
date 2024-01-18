41ec3c37d8a388a4444acb7b9c93be761938a346
Sandeep <vijeta013@gmail.com>
2024-01-17 23:14:08 -0500

    Time: 36 ms (65.15%) | Memory: 17.3 MB (15.77%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,17 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # mem = [0]*(n+1)
-        # mem[0] = 1
-        # for i in range(1,n+1):
-        #     mem[i] += mem[i-1]
-        #     if i-2 >= 0:
-        #         mem[i] += mem[i-2] 
-            
-        # return mem[n]
-        pre = 1
-        cur = 1
-        for i in range(1, n):
-            temp = cur+pre
-            pre = cur
-            cur = temp
-        return cur
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        one = 1
+        two = 2
+        
+        for i in range(3, n+1):
+            temp = one + two
+            one = two
+            two = temp
+        return two
