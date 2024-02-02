d1c0f7eebfd302606e21a486054314bb573c09d0
Sandeep <vijeta013@gmail.com>
2024-02-01 21:31:12 -0500

    Time: 38 ms (49.38%) | Memory: 16.6 MB (63.35%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        def createDigit(start, length):
+            num = 0
+            for i in range(length):
+                num += (start + i) * 10**(length - i-1)
+            return num
+        all_nums = []
+        for i in range(2, 10):
+            for j in range(1, 11-i):
+                all_nums.append(createDigit(j, i))
+        # print(all_nums)
+        res = []
+        for num in all_nums:
+            if num >= low and num <= high:
+                res.append(num)
+        return res
+
+        
